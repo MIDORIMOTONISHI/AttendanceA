67a173b13dfc58dffca1c1ced05d499bf6a509cd
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-04-22 04:48:56 +0000

    ユーザ新規作成まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only:[:show]
+
   def new
+    @user = User.new
   end
+  
+  def show
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザを新規作成しました。"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :code, :password, :password_confirmation)
+    end
 end
