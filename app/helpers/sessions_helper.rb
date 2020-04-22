9b7f0769aa76f994f5505ad64e5bc449b2dec390
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-04-22 14:19:47 +0000

    永続的セッションまで

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # 永続的セッションを記憶(詳細はUserモデル)
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   # 現在ログイン中のユーザがいればオブジェクトを返す
   def current_user
     if session[:user_id]
