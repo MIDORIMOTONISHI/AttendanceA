83f7ec3dbf6dcf1906e66513e9478c51b52af503
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-05-04 15:08:17 +0000

    出勤日数まで

---
+++ db/migrate/20200425161001_create_attendances.rb
@@ -0,0 +1,18 @@
+class CreateAttendances < ActiveRecord::Migration[5.1]
+  def change
+    create_table :attendances do |t|
+      t.date :worked_on
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.datetime :company_time
+      t.string :note
+      t.datetime :scheduled_end_time
+      t.datetime :overtime
+      t.string :business_process
+      t.string :confirmation
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200425102436) do
+ActiveRecord::Schema.define(version: 20200425161001) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.date "worked_on"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.datetime "company_time"
+    t.string "note"
+    t.datetime "scheduled_end_time"
+    t.datetime "overtime"
+    t.string "business_process"
+    t.string "confirmation"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,10 +38,9 @@ ActiveRecord::Schema.define(version: 20200425102436) do
     t.string "code"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.datetime "basic_time", default: "2020-04-24 23:00:00"
-    t.datetime "work_time", default: "2020-04-24 22:30:00"
-    t.datetime "basic_time_2", default: "2020-04-24 23:00:00"
-    t.datetime "work_f_time", default: "2020-04-25 07:30:00"
+    t.datetime "basic_time", default: "2020-05-03 23:00:00"
+    t.datetime "work_time", default: "2020-05-03 22:30:00"
+    t.datetime "work_f_time", default: "2020-05-04 07:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
