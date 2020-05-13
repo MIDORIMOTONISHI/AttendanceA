3857e1837e8afb309d8d5e735b215e51bc2ac53d
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-05-13 14:02:04 +0000

    出勤中社員一覧まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :working]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :working]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
-  before_action :set_one_month, only: :show
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info, :working]
+  before_action :set_one_month, only: [:show, :working]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -57,6 +57,10 @@ class UsersController < ApplicationController
     else
       render :edit_basic_info
     end
+  end
+  
+  def working
+    @users = User.all.includes(:attendances)
   end
 
     private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200508032314) do
+ActiveRecord::Schema.define(version: 20200513074729) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20200508032314) do
     t.datetime "work_time", default: "2020-05-07 22:30:00"
     t.datetime "work_f_time", default: "2020-05-08 07:30:00"
     t.boolean "boss", default: false
+    t.integer "number"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
